db1b070e703e658ab1db10ec2d3beaaa14b293f2
Yopi <myopisugara23@gmail.com>
2025-11-24 12:26:34 +0800

    fix: routing & main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Router } from "wouter";
+import { Router, Route } from "wouter";
 import App from "./App";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router base="/Nexxportv2">
-      <App />
-    </Router>
+      <Router>
+  <Route path="/login">
+    <Login />
+  </Route>
+
+  <Route path="/">
+    <Dashboard />
+  </Route>
+</Router>
+
   </React.StrictMode>
 );
